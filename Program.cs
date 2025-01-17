09d0f5094fa63aa6cf2382d3015987a2b2c23a34
Vlad Țurcanu <163206319+vladturcanu0usv@users.noreply.github.com>
2025-01-17 22:36:58 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Game gm = new Game(1280,720))
+            {
+                gm.Run();
+            }
+        }
+    }
+}
